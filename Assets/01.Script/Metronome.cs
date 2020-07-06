21a2c4de7f5eeaf9e6dd69e3a641d1904bd53e2f
Komorio <twilivez@gmail.com>
2020-07-06 19:30:40 +0900

    [ADD] 기본 메트로놈 추가

---
+++ Assets/01.Script/Metronome.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioSource ticSource;
+
+    [Header("Values")]
+    [SerializeField]
+    private double offset;
+
+    [SerializeField]
+    private double bpm;
+
+    [SerializeField]
+    private int split;
+
+    private double oneBeatTime;
+    private double nextSample;
+
+    private void Awake(){
+        double offsetForSample = offset * audioSource.clip.frequency;
+        oneBeatTime = (60.0 / bpm);
+
+        nextSample = oneBeatTime * audioSource.clip.frequency - offsetForSample;
+
+        audioSource.clip.frequency.Log();
+        audioSource.clip.samples.Log();
+        
+        (audioSource.clip.samples / audioSource.clip.frequency).Log();
+    }
+
+    private void Update(){
+        audioSource.timeSamples.Log();
+        if(audioSource.timeSamples >= nextSample){
+            StartCoroutine(TicSFX());
+        }
+    }
+
+    private IEnumerator TicSFX(){
+        ticSource.Play();
+        nextSample += oneBeatTime * audioSource.clip.frequency;
+
+        yield return null;
+    }
+}
